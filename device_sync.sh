38fe742dbe85bdc2c36ed650e505613c49d116ce
Emma Anderson <workeandrs@gmail.com>
2026-02-21 10:20:22 -0800

    Sync

---
+++ device_sync.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# sync saves from thalassa + Pixel
+# only modify synced files that are newer
+# sync changes to Git
+
+printf "What device did you last save to? [{C}onsole/{M}obile] "
+read i_response
+
+mkdir /tmp/dolphin-sync /tmp/dolphin-sync/console /tmp/dolphin-sync/mobile
+
+case $i_response in
+C*)
+  rsync -aurptv 192.168.0.183:.local/share/dolphin-emu/GC/* $HOME/remote-repos/saves/GC/
+  rsync -aurptv 192.168.0.183:.local/share/dolphin-emu/Wii/*  $HOME/remote-repos/saves/Wii/
+;;
+M*)
+  adb pull /sdcard/Android/data/org.dolphinemu.dolphinemu/files/GC $HOME/remote-repos/saves/
+  adb pull /sdcard/Android/data/org.dolphinemu.dolphinemu/files/Wii $HOME/remote-repos/saves/
+;;
+esac
+
+cd remote-repos/saves
+git add *
+git commit -m "Sync"
+git push
